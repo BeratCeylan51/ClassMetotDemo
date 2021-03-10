bf2861064b1dfc10ffd6ff7f47193c03a8c01658
BeratCeylan515151 <berat51ceylan@gmail.com>
2021-03-10 23:41:50 +0300

    Add project files.

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine(customer.Name + " " + customer.Surname + " Müşteri Olarak Eklendi");
+        }
+
+        public void Lists(Customer[] customers)
+        {
+            foreach (Customer customer in customers)
+            {
+                Console.WriteLine(customer.Id);
+                Console.WriteLine(customer.Name);
+                Console.WriteLine(customer.Surname);
+                Console.WriteLine(customer.CityOfResidence);
+                Console.WriteLine(customer.age);
+                Console.WriteLine("--------------------");
+            }
+        }
+
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine(customer.Name + " " + customer.Surname + " Müşterisi Silindi");
+        }
+    }
+}
